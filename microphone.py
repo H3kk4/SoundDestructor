1d19a386db66335698b546430274d5bc8ba6543a
Ad2rien5 <seach.adrien@gmail.com>
2026-05-09 11:13:48 +0200

    premier cas d'enregistrement

---
+++ microphone.py
@@ -0,0 +1,31 @@
+import sounddevice as sd
+import numpy as np
+from PyQt5.QtCore import QObject, pyqtSignal
+
+class AudioInput(QObject):
+
+    new_data = pyqtSignal(np.ndarray)
+
+    def __init__(self):
+        super().__init__()
+
+        self.stream = sd.InputStream(
+            channels=1,
+            samplerate=44100,
+            blocksize=512,
+            callback=self.audio_callback
+        )
+
+    def start(self):
+        self.stream.start()
+
+    def stop(self):
+        self.stream.stop()
+
+    def audio_callback(self, indata, frames, time, status):
+        if status:
+            print(status)
+
+        samples = indata[:, 0].copy()
+
+        self.new_data.emit(samples)
